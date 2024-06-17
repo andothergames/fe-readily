bc2d7ab3fcd314e00ed9412ce748d8e7601a190f
Joy <32204909+andothergames@users.noreply.github.com>
2024-06-17 10:23:42 +0100

    init: 01 create react repo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import icon from "/readily-icon.png";
+import "./App.css";
+
+function App() {
+  return (
+    <section>
+      <img src={icon} className="logo" alt="readily icon" />
+      <h1>Readily</h1>
+    </section>
+  );
+}
+
+export default App;
